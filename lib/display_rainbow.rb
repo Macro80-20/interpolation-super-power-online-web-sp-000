c43323bf9bb751992b229313c3e049420009239a
Fernando Lopez <fernando_lopezc@live.com>
2020-02-03 23:49:39 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
+def display_rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+ puts [0, 'R'], [1, 'O'], [2, 'Y'], [3, 'G'], [4, 'B'], [5, 'I'], [6, 'V']
+ end
 # Write your #display_rainbow method here
